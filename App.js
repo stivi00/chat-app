aea85ee2bd736166627e518a2f26f0e23bbc02a6
stivi00 <stivisth@gmail.com>
2023-11-05 17:02:31 +0100

    added firebase connection

--- App.js
+++ App.js
@@ -5,11 +5,28 @@ import Chat from './components/Chat.js';
 // import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: 'AIzaSyAWqOtR-19ZYiJqAuiU30aYSr3PZU1EfRs',
+        authDomain: 'chatapp-9cf6d.firebaseapp.com',
+        projectId: 'chatapp-9cf6d',
+        storageBucket: 'chatapp-9cf6d.appspot.com',
+        messagingSenderId: '740947276148',
+        appId: '1:740947276148:web:1831a743a7e632475d3c21',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Start'>
