ee2377c17a159274b8ed050d5038cf646363fee2
stivi00 <stivisth@gmail.com>
2023-11-07 18:13:04 +0100

    added function to listen for connection break

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ import Chat from './components/Chat.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import {
+    getFirestore,
+    disableNetwork,
+    enableNetwork,
+} from 'firebase/firestore';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
@@ -27,12 +32,30 @@ const App = () => {
 
     // Initialize Cloud Firestore and get a reference to the service
     const db = getFirestore(app);
+
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert('Connection lost!');
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected]);
+
+    const connectionStatus = useNetInfo();
+
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Start'>
                 <Stack.Screen name='Start' component={Start} />
                 <Stack.Screen name='Chat'>
-                    {(props) => <Chat db={db} {...props} />}
+                    {(props) => (
+                        <Chat
+                            isConnected={connectionStatus.isConnected}
+                            db={db}
+                            {...props}
+                        />
+                    )}
                 </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
