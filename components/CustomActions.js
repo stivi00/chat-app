d6ffcb6a7b8dbdff78de013bc693e227f9f7db35
stivi00 <stivisth@gmail.com>
2023-11-09 20:29:49 +0100

    added on action press function

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,7 +1,37 @@
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
-    const onActionPress = () => {};
+    const actionSheet = useActionSheet();
+
+    const onActionPress = () => {
+        const options = [
+            'Choose From Library',
+            'Take Picture',
+            'Send Location',
+            'Cancel',
+        ];
+        const cancelButtonIndex = options.length - 1;
+        actionSheet.showActionSheetWithOptions(
+            {
+                options,
+                cancelButtonIndex,
+            },
+            async (buttonIndex) => {
+                switch (buttonIndex) {
+                    case 0:
+                        console.log('user wants to pick an image');
+                        return;
+                    case 1:
+                        console.log('user wants to take a photo');
+                        return;
+                    case 2:
+                        console.log('user wants to get their location');
+                    default:
+                }
+            }
+        );
+    };
 
     return (
         <TouchableOpacity
