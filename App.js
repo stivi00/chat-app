2f64462bfb513cf94a71c7dc3b7ce34ad825b29e
stivi00 <stivisth@gmail.com>
2023-11-11 08:12:33 +0100

    app component refactoring

--- App.js
+++ App.js
@@ -1,46 +1,43 @@
-// import the screens
-import Start from './components/Start.js';
-import Chat from './components/Chat.js';
-
-import { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Alert } from 'react-native';
 import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
 import {
     getFirestore,
     disableNetwork,
     enableNetwork,
 } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNetInfo } from '@react-native-community/netinfo';
+import { Alert } from 'react-native';
+import Start from './components/Start';
+import Chat from './components/Chat';
+import { useEffect } from 'react';
 
-// Create the navigator
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyAWqOtR-19ZYiJqAuiU30aYSr3PZU1EfRs',
+    authDomain: 'chatapp-9cf6d.firebaseapp.com',
+    projectId: 'chatapp-9cf6d',
+    storageBucket: 'chatapp-9cf6d.appspot.com',
+    messagingSenderId: '740947276148',
+    appId: '1:740947276148:web:1831a743a7e632475d3c21',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+const storage = getStorage(app);
+
 const Stack = createNativeStackNavigator();
 
-const App = () => {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: 'AIzaSyAWqOtR-19ZYiJqAuiU30aYSr3PZU1EfRs',
-        authDomain: 'chatapp-9cf6d.firebaseapp.com',
-        projectId: 'chatapp-9cf6d',
-        storageBucket: 'chatapp-9cf6d.appspot.com',
-        messagingSenderId: '740947276148',
-        appId: '1:740947276148:web:1831a743a7e632475d3c21',
-    };
-
+export default function App() {
     const connectionStatus = useNetInfo();
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    // Initialize Cloud Firestore and get a reference to the service
-    const db = getFirestore(app);
-    // Initialize Firestore Storage
-    const storage = getStorage(app);
 
     useEffect(() => {
         if (connectionStatus.isConnected === false) {
-            Alert.alert('Connection lost!');
+            Alert.alert('Connection Lost!');
             disableNetwork(db);
         } else if (connectionStatus.isConnected === true) {
             enableNetwork(db);
@@ -64,6 +61,4 @@ const App = () => {
             </Stack.Navigator>
         </NavigationContainer>
     );
-};
-
-export default App;
+}
